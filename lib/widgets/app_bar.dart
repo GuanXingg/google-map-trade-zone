5c2a07be0c1028c46ceabac303b4d6d57d69da85
G24 <quanhung309.ht@gmail.com>
2023-06-16 14:36:40 +0700

    [refactor] - change logo

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 AppBar customAppBar({Widget? leading, List<Widget>? actions}) => AppBar(
-      title: Image.asset('assets/images/logo-symbol.png', width: 120),
+      title: Image.asset('assets/images/logo-symbol.png', width: 80),
       centerTitle: true,
       leading: leading,
       actions: actions,
